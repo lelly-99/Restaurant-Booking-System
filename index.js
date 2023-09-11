18d95b01cbe673d0d61f7c9f44c8b8f64ba0650b
Progress Lefsifi <106746065+lelly-99@users.noreply.github.com>
2023-09-11 10:14:40 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,49 @@
+//epress
 import express from "express";
-import pgp from "pg-promise";
+//pg promise
+import pgPromise from "pg-promise";
+//handlebars
 import exphbs from "express-handlebars";
+//bodyparser
 import bodyParser from "body-parser";
-import flash from "flash-express";
+//flash
+import flash from "connect-flash";
+//sessions
+import session from "express-session";
+//database
+import restaurant from "./services/restaurant";
 
 const app = express()
 
+const DATABASE_URL = 'postgres://qgtbvckp:rvclmk79eXhageyYs9QHoyz7UBXFeVK8@silly.db.elephantsql.com/qgtbvckp?ssl=true';
+
+const connectionString = process.env.DATABASE_URL || DATABASE_URL;
+const db = pgPromise()(connectionString);
+
+
+
+app.use(
+    session({
+      secret: "resturant",
+      resave: false,
+      saveUninitialized: true,
+    })
+  );
 app.use(express.static('public'));
 app.use(flash());
-
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-
 const handlebarSetup = exphbs.engine({
     partialsDir: "./views/partials",
     viewPath: './views',
     layoutsDir: './views/layouts'
 });
-
 app.engine('handlebars', handlebarSetup);
 app.set('view engine', 'handlebars');
+app.use(function (req, res, next) {
+    res.locals.messages = req.flash();
+    next();
+  });
 
 app.get("/", (req, res) => {
 
@@ -32,7 +56,7 @@ app.get("/bookings", (req, res) => {
 });
 
 
-var portNumber = process.env.PORT || 3000;
+var portNumber = process.env.PORT || 3007;
 
 //start everything up
 app.listen(portNumber, function () {
